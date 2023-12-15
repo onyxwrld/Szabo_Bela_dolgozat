5dd3a7e3cf22059b42098b114877342df3269816
Petrik Diak <diak@petrik.hu>
2023-12-15 10:27:31 +0100

    final comit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
 import * as mysql from 'mysql2';
 import { AppService } from './app.service';
-
+import { kuponDTO } from './kuponDTO';
+import e, {Response} from 'express';
 const conn = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT) || 3306,
@@ -16,7 +17,50 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  async index() {
+    const [ adatok ] = await conn.execute('SELECT id, title, percentage, code FROM szinhaz ORDER BY title');
+    console.log(adatok);
+    return {
+      cupons: adatok, 
+     };
   }
+  @Get('/newCoupon')
+  @Render('newCoupon')
+  newMusic() {
+    return { hiba: '' };
+  }
+
+  @Post('/newCoupon')
+  @Render('newCoupon')
+  async addNewMusic(@Body() newCoupon: kuponDTO, @Res() res: Response) {
+      const title = newCoupon.title;
+      const percentage = parseInt(newCoupon.percentage);
+      const code = newCoupon.code;
+      const regex: RegExp = /^[A-Z]{4}-\d{6}$/;
+      console.log(percentage);
+      if(title.trim() == "" || percentage.toString().trim() == "" || code.toString().trim() == "") {
+        return { hiba: "Minden mezőt kötelező kitölteni!"};
+      } else if(regex.test(code)!=true) 
+      {
+        return { hiba: "Nem megfelelő kód formátum!"};
+      }
+      else if(title.length<1) 
+      {
+        return { hiba: "Több karakteres cimet adjon meg!"};
+      }
+      else if(!(percentage<=99 && percentage>=1)) 
+      {
+        return { hiba: "Nem megfelelő százalék érték!"};
+      }
+      else
+      {
+        const [ adatok ] = await conn.execute('INSERT INTO szinhaz (title, percentage, code) VALUES (?, ?, ?)', [ 
+          title,
+          percentage,
+          code,
+        ],
+        );
+        res.redirect('/');
+      }
+    }
 }
---
+++ src/kuponDTO.ts
@@ -0,0 +1,5 @@
+export class kuponDTO {
+  title:string;
+  percentage:string;
+  code:string;
+}
